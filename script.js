9ce8dd36630ffc3ea34829987b2550cacdf2f92c
ahmeteid <ahmeteid355@gmail.com>
2024-06-23 03:43:32 +0300

    project completed

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let inputMonth = document.querySelector("#date-info");
 let showMonth = document.querySelector(".show-month");
 let inputYear = document.querySelector(".years");
 let showYear = document.querySelector(".show-year");
+let inputCvc = document.querySelector("#cvc");
+let showCvc = document.querySelector(".cvc");
 
 inputName.onkeyup = function () {
   if (inputName.value === "" || !isNaN(inputName.value)) {
@@ -31,5 +33,36 @@ inputMonth.onkeyup = function () {
     showMonth.innerHTML = "0" + inputMonth.value;
   } else {
     showMonth.innerHTML = inputMonth.value;
+  }
+};
+
+inputYear.onkeyup = function () {
+  if (inputYear.value === "") {
+    showYear.innerHTML = "00";
+  }
+  if (inputYear.value < 10) {
+    showYear.innerHTML = "0" + inputYear.value;
+  } else {
+    showYear.innerHTML = inputYear.value;
+  }
+};
+
+inputCvc.onkeyup = function () {
+  if (inputCvc.value === "" || inputCvc.value === !NaN) {
+    showCvc.innerHTML = "000";
+  } else {
+    showCvc.innerHTML = inputCvc.value;
+  }
+};
+
+document.forms[0].onsubmit = function (e) {
+  if (
+    inputName.value === "" ||
+    inputNumber.value === "" ||
+    inputMonth.value === "" ||
+    inputYear.value === "" ||
+    inputCvc.value === ""
+  ) {
+    e.preventDefault();
   }
 };
